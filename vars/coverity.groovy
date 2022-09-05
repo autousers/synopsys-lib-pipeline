69410c9c41366f97601b4d9c9e5f16fea2ce0683
pemapol <t.pemapol@gmail.com>
2022-09-05 16:51:05 +0700

    create function capture

--- vars/coverity.groovy
+++ vars/coverity.groovy
@@ -1,5 +1,15 @@
 #!/usr/bin/env groovy
 
-def covBuildless() {
-    
+def capture(projectName, streamName) {
+    withCoverityEnvironment(coverityInstanceUrl: 'http://10.103.3.36:8080', createMissingProjectsAndStreams: true, projectName: "${projectName}", streamName: "${streamName}", viewName: 'High Impact Outstanding') {
+        sh """
+            cov-configure --python
+            cov-capture --dir idir --source-dir %pwd%
+            cov-analyze --dir idir --all --webapp-security --distrust-all --strip-path %pwd%
+            cov-commit-defects --dir idir --url $COV_URL --stream $COV_STREAM
+        """
+    }
+}
+
+def build() {
 }
