c51d1798dd77b97dc0cc761061a23095c2c10f19
pemapol <t.pemapol@gmail.com>
2022-09-06 14:19:42 +0700

    update func capture

--- vars/coverity.groovy
+++ vars/coverity.groovy
@@ -1,11 +1,10 @@
 #!/usr/bin/env groovy
 
-def capture(projectName, streamName) {
-    withCoverityEnvironment(coverityInstanceUrl: 'http://10.103.3.36:8080', createMissingProjectsAndStreams: true, projectName: "${projectName}", streamName: "${streamName}", viewName: 'High Impact Outstanding') {
+def capture(coverityURL, projectName, streamName, sourcePath) {
+    withCoverityEnvironment(coverityInstanceUrl: "${coverityURL}", createMissingProjectsAndStreams: true, projectName: "${projectName}", streamName: "${streamName}", viewName: 'High Impact Outstanding') {
         sh """
-            cov-configure --python
-            cov-capture --dir idir --source-dir %pwd%
-            cov-analyze --dir idir --all --webapp-security --distrust-all --strip-path %pwd%
+            cov-capture --dir idir --source-dir ${sourcePath}
+            cov-analyze --dir idir --all --webapp-security --distrust-all --strip-path ${sourcePath}
             cov-commit-defects --dir idir --url $COV_URL --stream $COV_STREAM
         """
     }
