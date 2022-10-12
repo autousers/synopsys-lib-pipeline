7b73b5ad2a2fb566e02ed83b3b9ec3e9654f556f
pemapol <t.pemapol@gmail.com>
2022-10-12 13:02:32 +0700

    edit strip path from source path to workspace

--- vars/coverity.groovy
+++ vars/coverity.groovy
@@ -4,7 +4,7 @@ def capture(coverityURL, projectName, streamName, sourcePath) {
     withCoverityEnvironment(coverityInstanceUrl: "${coverityURL}", createMissingProjectsAndStreams: true, projectName: "${projectName}", streamName: "${streamName}", viewName: 'High Impact Outstanding') {
         sh """
             cov-capture --dir idir --source-dir ${sourcePath}
-            cov-analyze --dir idir --all --webapp-security --distrust-all --strip-path ${sourcePath}
+            cov-analyze --dir idir --all --webapp-security --distrust-all --strip-path $WORKSPACE
             cov-commit-defects --dir idir --url $COV_URL --stream $COV_STREAM
         """
     }
@@ -14,7 +14,7 @@ def build(coverityURL, projectName, streamName, buildCmd) {
     withCoverityEnvironment(coverityInstanceUrl: "${coverityURL}", createMissingProjectsAndStreams: true, projectName: "${projectName}", streamName: "${streamName}", viewName: 'High Impact Outstanding') {
         sh """
             cov-build --dir idir --fs-capture-search . ${buildCmd}
-            cov-analyze --dir idir --all --webapp-security --distrust-all --strip-path ${sourcePath}
+            cov-analyze --dir idir --all --webapp-security --distrust-all --strip-path $WORKSPACE
             cov-commit-defects --dir idir --url $COV_URL --stream $COV_STREAM
         """
     }
