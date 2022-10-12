aff6ccc86b69ad9bb45f411dc07b12b23512ea29
pemapol <t.pemapol@gmail.com>
2022-10-12 12:58:57 +0700

    add build function

--- vars/coverity.groovy
+++ vars/coverity.groovy
@@ -10,5 +10,12 @@ def capture(coverityURL, projectName, streamName, sourcePath) {
     }
 }
 
-def build() {
+def build(coverityURL, projectName, streamName, buildCmd) {
+    withCoverityEnvironment(coverityInstanceUrl: "${coverityURL}", createMissingProjectsAndStreams: true, projectName: "${projectName}", streamName: "${streamName}", viewName: 'High Impact Outstanding') {
+        sh """
+            cov-build --dir idir --fs-capture-search . ${buildCmd}
+            cov-analyze --dir idir --all --webapp-security --distrust-all --strip-path ${sourcePath}
+            cov-commit-defects --dir idir --url $COV_URL --stream $COV_STREAM
+        """
+    }
 }
