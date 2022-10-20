abf0f5b8b4cefcc0468f865f833d24dbfe854e6a
pemapol <t.pemapol@gmail.com>
2022-10-20 20:31:58 +0700

    edit detect

--- vars/blackduck.groovy
+++ vars/blackduck.groovy
@@ -1,8 +1,7 @@
 #!/usr/bin/env groovy
 
-def scanImage(blackduck_URL, blackduck_Token, blackduck_ProjectName, blackduck_Version, blackduck_scanName, imageName) {
-    synopsys_detect detectProperties: """
-    --detect.project.name=${blackduck_ProjectName} 
+def scanImage(blackduck_ProjectName, blackduck_Version, blackduck_scanName, imageName) {
+    synopsys_detect detectProperties: """--detect.project.name=${blackduck_ProjectName} 
     --detect.project.version.name=${blackduck_Version} 
     --detect.code.location.name=${blackduck_scanName} 
     --detect.target.type=IMAGE 
