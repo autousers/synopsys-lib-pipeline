40cdd65e8ba45c67bbfd65d9f54381415e6030f5
pemapol <t.pemapol@gmail.com>
2022-10-20 18:03:27 +0700

    add detect scan source

--- vars/blackduck.groovy
+++ vars/blackduck.groovy
@@ -10,3 +10,13 @@ def scanImage(blackduck_URL, blackduck_Token, blackduck_ProjectName, blackduck_V
     --detect.docker.image=${imageName} \\
     --detect.tools.excluded=BINARY_SCAN''', downloadStrategyOverride: [$class: 'ScriptOrJarDownloadStrategy']
 }
+
+def scanSource(blackduck_URL, blackduck_Token, blackduck_ProjectName, blackduck_Version, blackduck_scanName, blackduck_sourcePath) {
+    synopsys_detect detectProperties: '''--blackduck.url=${blackduck_URL} \\
+    --blackduck.api.token="${blackduck_Token}" \\
+    --detect.project.name=${blackduck_ProjectName} \\
+    --detect.project.version.name=${blackduck_Version} \\
+    --detect.code.location.name=${blackduck_scanName} \\
+    --detect.source.path=${blackduck_sourcePath} \\
+    --detect.tools.excluded=BINARY_SCAN''', downloadStrategyOverride: [$class: 'ScriptOrJarDownloadStrategy']
+}
