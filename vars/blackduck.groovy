98bdc4af07d5daee27c3172148e5881001550c66
pemapol <t.pemapol@gmail.com>
2022-10-20 18:43:28 +0700

    edit func detect scan source

--- vars/blackduck.groovy
+++ vars/blackduck.groovy
@@ -12,11 +12,11 @@ def scanImage(blackduck_URL, blackduck_Token, blackduck_ProjectName, blackduck_V
 }
 
 def scanSource(blackduck_URL, blackduck_Token, blackduck_ProjectName, blackduck_Version, blackduck_scanName, blackduck_sourcePath) {
-    synopsys_detect detectProperties: '''--blackduck.url=${blackduck_URL} \\
+    synopsys_detect detectProperties: """--blackduck.url=${blackduck_URL} \\
     --blackduck.api.token="${blackduck_Token}" \\
     --detect.project.name=${blackduck_ProjectName} \\
     --detect.project.version.name=${blackduck_Version} \\
     --detect.code.location.name=${blackduck_scanName} \\
     --detect.source.path=${blackduck_sourcePath} \\
-    --detect.tools.excluded=BINARY_SCAN''', downloadStrategyOverride: [$class: 'ScriptOrJarDownloadStrategy']
+    --detect.tools.excluded=BINARY_SCAN""", downloadStrategyOverride: [$class: 'ScriptOrJarDownloadStrategy']
 }
