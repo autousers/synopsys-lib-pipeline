f5771309f59707e5e66d6dd201f286bd07edfaa0
pemapol <t.pemapol@gmail.com>
2022-10-20 20:31:03 +0700

    edit detect

--- vars/blackduck.groovy
+++ vars/blackduck.groovy
@@ -1,14 +1,13 @@
 #!/usr/bin/env groovy
 
 def scanImage(blackduck_URL, blackduck_Token, blackduck_ProjectName, blackduck_Version, blackduck_scanName, imageName) {
-    synopsys_detect detectProperties: '''--blackduck.url=${blackduck_URL} \\
-    --blackduck.api.token="${blackduck_Token}" \\
-    --detect.project.name=${blackduck_ProjectName} \\
-    --detect.project.version.name=${blackduck_Version} \\
-    --detect.code.location.name=${blackduck_scanName} \\
-    --detect.target.type=IMAGE \\
-    --detect.docker.image=${imageName} \\
-    --detect.tools.excluded=BINARY_SCAN''', downloadStrategyOverride: [$class: 'ScriptOrJarDownloadStrategy']
+    synopsys_detect detectProperties: """
+    --detect.project.name=${blackduck_ProjectName} 
+    --detect.project.version.name=${blackduck_Version} 
+    --detect.code.location.name=${blackduck_scanName} 
+    --detect.target.type=IMAGE 
+    --detect.docker.image=${imageName} 
+    --detect.tools.excluded=BINARY_SCAN""", downloadStrategyOverride: [$class: 'ScriptOrJarDownloadStrategy']
 }
 
 def scanSource(blackduck_ProjectName, blackduck_Version, blackduck_scanName, blackduck_sourcePath) {
